df7a9f82cafb49181cd3b4010e0cfb61850aa6b4
Nauer05 <lucasnauer@gmail.com>
2024-04-18 10:33:59 -0300

    Commit Nauer

---
+++ CPTM/src/cptm/Linha.java
@@ -0,0 +1,49 @@
+package cptm;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Linha {
+	
+    private String nome;
+    private List<Trem> trens;
+    private List<Estacao> estacoes;
+    
+    public Linha(String nome) {
+        this.nome = nome;
+        this.trens = new ArrayList<>();
+        this.estacoes = new ArrayList<>();
+    }
+   
+    public void addTrain(Trem trem) {
+        this.trens.add(trem);
+    }
+    
+    public void addEstacao(Estacao Estacao) {
+        this.estacoes.add(Estacao);
+    }
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public List<Trem> getTrens() {
+		return trens;
+	}
+
+	public void setTrens(List<Trem> trens) {
+		this.trens = trens;
+	}
+
+	public List<Estacao> getEstacoes() {
+		return estacoes;
+	}
+
+	public void setEstacoes(List<Estacao> estacoes) {
+		this.estacoes = estacoes;
+	}    
+}
