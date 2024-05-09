455209cb6aede315a0eb67ac600c6d90d62e855e
FerLaragnoit <laragnoit20@gmail.com>
2024-05-09 11:03:39 -0300

    current files

--- CPTM/src/cptm/Estacao.java
+++ CPTM/src/cptm/Estacao.java
@@ -1,17 +1,11 @@
 package cptm;
 
-<<<<<<< HEAD
-=======
-import java.util.ArrayList;
-
->>>>>>> 24f790473d1505e99a1e3a29f4f26e4685d7761f
 public class Estacao {
 	
 	private String id;
 	private String nome;
 	private String localizacao;
 	private String linha;
-<<<<<<< HEAD
 	private boolean status;
 	private boolean ocupadoLadoA;
 	private boolean ocupadoLadoB;
@@ -20,24 +14,15 @@ public class Estacao {
 	public Estacao() {}
 	
 	public Estacao(String id, String nome, String localizacao, String linha, boolean status, boolean ocupadoLadoA, boolean ocupadoLadoB, boolean ocorrencia) {
-=======
-	
-	public Estacao() {}
-	
-	public Estacao(String id, String nome, String localizacao, String linha) {
->>>>>>> 24f790473d1505e99a1e3a29f4f26e4685d7761f
 		super();
 		this.id = id;
 		this.nome = nome;
 		this.localizacao = localizacao;
 		this.linha = linha;
-<<<<<<< HEAD
 		this.status = status;
 		this.ocupadoLadoB = ocupadoLadoA;
 		this.ocupadoLadoB = ocupadoLadoB;
 		this.ocorrencia = ocorrencia;
-=======
->>>>>>> 24f790473d1505e99a1e3a29f4f26e4685d7761f
 	}
 
 	public String getId() {
@@ -72,7 +57,6 @@ public class Estacao {
 		this.linha = linha;
 	}
 	
-<<<<<<< HEAD
 	public boolean isStatus() {
 		return status;
 	}
@@ -103,17 +87,5 @@ public class Estacao {
 
 	public void setOcorrencia(boolean ocorrencia) {
 		this.ocorrencia = ocorrencia;
-=======
-	public static void cadastraEstacao(Estacao estacao, ArrayList<Estacao> lista) {
-		lista.add(estacao);
-	}
-	
-	public static void removeEstacao(Estacao estacao, ArrayList<Estacao> lista) {
-		lista.remove(estacao);
-	}
-	
-	public void add(Object item, ArrayList<Object> lista) {
-		lista.add(item);
->>>>>>> 24f790473d1505e99a1e3a29f4f26e4685d7761f
 	}
 }
